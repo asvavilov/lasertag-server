2578fdd654af6896592071ac2b3e9c0ee81e61f7
asvavilov <asvavilov@yandex.ru>
2024-09-24 20:35:35 +0000

    идентификаторы в окружении и пример отправки

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+/*
+app.get("/ronin-bot", (req: Request, res: Response) => {
+	console.log(process.env)
+	const url = `https://api.telegram.org/bot${process.env.BOT_ID}:${process.env.BOT_SECRET}/sendMessage`;
+	const text = 'hello from express';
+	console.log(url)
+	fetch(url, {
+		method: 'POST',
+		headers: {
+      'Content-Type': 'application/json'
+    },
+		body: JSON.stringify({
+			chat_id: process.env.TEST_GROUP,
+			text: text
+		})
+	});
 	res.send();
 });
+*/
 
 app.post("/ronin-bot", (req: Request, res: Response) => {
 	//console.log(req);
